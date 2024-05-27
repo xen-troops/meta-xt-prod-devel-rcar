xen-troops/meta-xt-prod-devel-rcar

commit 4c277f4f00095bb6875a1d82b51c63ef74704bbd

dom-0: update xen config for dom-u
Update dom-u xen configuration for 'virtio' build configuration.

For virtio functionality, we need to use different xen configuration
in comparison with the non-virtio build.

This patch introduces dynamic filling of the dom-u's xen configuration
depending on 'enable_virtio' distro feature.

Signed-off-by: Dmytro Terletskyi <dmytro_terletskyi@epam.com>
Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

meta-xt-prod-devel-rcar-control/recipes-guests/domu/domu.bbappend
@@ -7,6 +7,7 @@ SRC_URI += "\
     file://domu-set-root \
     ${@bb.utils.contains('DISTRO_FEATURES', 'sndbe', 'file://sndbe-backend.conf', '', d)} \
     ${@bb.utils.contains('DISTRO_FEATURES', 'displbe', 'file://displbe-backend.conf', '', d)} \
+    file://pvr-${XT_DOMU_CONFIG_NAME} \
 "
 
 FILES:${PN} += " \
@@ -18,8 +19,24 @@ FILES:${PN} += " \
 # It is used a lot in the do_install, so variable will be handy
 CFG_FILE="${D}${sysconfdir}/xen/domu.cfg"
 
+CMD_LINE_ROOT_DEVICE = "xvda1"
+CMD_LINE_ROOT_DEVICE:enable_virtio = "vda"
+
+CMD_LINE_PVR = "pvrsrvkm.DriverMode=1"
+CMD_LINE_PVR:enable_virtio = ""
+
+CMD_LINE_EXTRA = "\"root=/dev/${CMD_LINE_ROOT_DEVICE} rw rootwait console=hvc0 cma=256M ${CMD_LINE_PVR}\""
+
+MAX_GRANT_FRAMES = "64"
+MAX_GRANT_FRAMES:enable_virtio = "512"
+
+VIRTIO_QEMU_DOMID = "1"
+
+
 do_install:append() {
-    cat ${WORKDIR}/domu-vdevices.cfg >> ${CFG_FILE}
+
+    echo "extra = ${CMD_LINE_EXTRA}" >> ${CFG_FILE}
+    echo "max_grant_frames = ${MAX_GRANT_FRAMES}" >> ${CFG_FILE}
 
     if ${@bb.utils.contains('DISTRO_FEATURES', 'pvcamera', 'true', 'false', d)}; then
         cat ${WORKDIR}/domu-pvcamera.cfg >> ${CFG_FILE}
@@ -28,8 +45,13 @@ do_install:append() {
         echo "After=backend-ready@camerabe.service" >> ${D}${systemd_unitdir}/system/domu.service
     fi
 
-    if ${@bb.utils.contains('DISTRO_FEATURES', 'enable_virtio', 'false', 'true', d)}; then
-        echo "device_tree = \"/usr/lib/xen/boot/domu.dtb\"" >> ${CFG_FILE}
+    if ${@bb.utils.contains('DISTRO_FEATURES', 'enable_virtio', 'true', 'false', d)}; then
+        # Create virtio related device-tree nodes (virtio-mmio and PCI host bridge),
+        # use "0" for Xen foreign mappings, and Qemu domid for Xen grant mappings
+        echo "virtio_qemu_domid = ${VIRTIO_QEMU_DOMID}" >> ${CFG_FILE}
+    else
+        cat ${WORKDIR}/domu-vdevices.cfg >> ${CFG_FILE}
+        cat ${WORKDIR}/pvr-${XT_DOMU_CONFIG_NAME} >> ${CFG_FILE}
     fi
 
     # Install domu-set-root script

meta-xt-prod-devel-rcar-control/recipes-guests/domu/files/domu-generic-h3-4x2g.cfg
@@ -10,33 +10,11 @@ name = "DomU"
 # Kernel image to boot
 kernel = "/usr/lib/xen/boot/linux-domu"
 
-dtdev = [
-    "/soc/gsx_pv0_domu",
-    "/soc/gsx_pv1_domu",
-    "/soc/gsx_pv2_domu",
-    "/soc/gsx_pv3_domu",
-]
-
-# Kernel command line options
-# In case of using virtio-blk device this should be "root=/dev/vda"
-extra = "root=/dev/xvda1 rw rootwait console=hvc0 cma=256M pvrsrvkm.DriverMode=1"
-
 # Initial memory allocation (MB)
 memory = 1536
 
-max_grant_frames = 64
-
 # Number of VCPUS
 vcpus = 4
-
-irqs = [
-    151
-]
-
-iomem = [
-#gsx@fd000000
-    "0xfd010,1@0xfd000",
- ]
 
 on_crash = 'preserve'
 

meta-xt-prod-devel-rcar-control/recipes-guests/domu/files/domu-generic-m3-2x4g.cfg
@@ -10,31 +10,11 @@ name = "DomU"
 # Kernel image to boot
 kernel = "/usr/lib/xen/boot/linux-domu"
 
-dtdev = [
-    "/soc/gsx_pv0_domu",
-    "/soc/gsx_pv1_domu",
-]
-
-# Kernel command line options
-# In case of using virtio-blk device this should be "root=/dev/vda"
-extra = "root=/dev/xvda1 rw rootwait console=hvc0 cma=256M pvrsrvkm.DriverMode=1"
-
 # Initial memory allocation (MB)
 memory = 1536
 
-max_grant_frames = 64
-
 # Number of VCPUS
 vcpus = 4
-
-irqs = [
-    151
-]
-
-iomem = [
-#gsx@fd000000
-    "0xfd010,1@0xfd000",
- ]
 
 on_crash = 'preserve'
 

meta-xt-prod-devel-rcar-control/recipes-guests/domu/files/domu-salvator-xs-m3n.cfg
@@ -10,28 +10,11 @@ name = "DomU"
 # Kernel image to boot
 kernel = "/usr/lib/xen/boot/linux-domu"
 
-# Kernel command line options
-# In case of using virtio-blk device this should be "root=/dev/vda"
-extra = "root=/dev/xvda1 rw rootwait console=hvc0 cma=128M pvrsrvkm.DriverMode=1"
-
 # Initial memory allocation (MB)
 memory = 512
 
-max_grant_frames = 64
-
 # Number of VCPUS
 vcpus = 2
-
-dtdev = [ "/soc/gsx_domu" ]
-
-irqs = [
-    151
-]
-
-iomem = [
-#gsx@fd000000
-    "0xfd010,1@0xfd000",
- ]
 
 on_crash = 'preserve'
 

meta-xt-prod-devel-rcar-control/recipes-guests/domu/files/pvr-domu-generic-h3-4x2g.cfg
@@ -1,0 +1,17 @@
+device_tree = "/usr/lib/xen/boot/domu.dtb"
+
+dtdev = [
+    "/soc/gsx_pv0_domu",
+    "/soc/gsx_pv1_domu",
+    "/soc/gsx_pv2_domu",
+    "/soc/gsx_pv3_domu",
+]
+
+irqs = [
+    151
+]
+
+iomem = [
+#gsx@fd000000
+    "0xfd010,1@0xfd000",
+ ]

meta-xt-prod-devel-rcar-control/recipes-guests/domu/files/pvr-domu-generic-m3-2x4g.cfg
@@ -1,0 +1,15 @@
+device_tree = "/usr/lib/xen/boot/domu.dtb"
+
+dtdev = [
+    "/soc/gsx_pv0_domu",
+    "/soc/gsx_pv1_domu",
+]
+
+irqs = [
+    151
+]
+
+iomem = [
+#gsx@fd000000
+    "0xfd010,1@0xfd000",
+ ]

meta-xt-prod-devel-rcar-control/recipes-guests/domu/files/pvr-domu-salvator-xs-m3n.cfg
@@ -1,0 +1,12 @@
+device_tree = "/usr/lib/xen/boot/domu.dtb"
+
+dtdev = [ "/soc/gsx_domu" ]
+
+irqs = [
+    151
+]
+
+iomem = [
+#gsx@fd000000
+    "0xfd010,1@0xfd000",
+ ]

commit 22002de0f23ee189bb5454df17a62f73aee7e8bd

dom0: wrap usage of the XT_DOMU_CONFIG_NAME
The variable `XT_DOMU_CONFIG_NAME` is not defined if domu is not used.
In such a case, yocto fails during the parsing of the `domu.bbappend`
file due to an attempt to calculate the checksum of the non-existing
file `pvr-${XT_DOMU_CONFIG_NAME}`.

Fixes: 4c277f4f0009 ("dom-0: update xen config for dom-u")

Signed-off-by: Ruslan Shymkevych <ruslan_shymkevych@epam.com>
Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

meta-xt-prod-devel-rcar-control/recipes-guests/domu/domu.bbappend
@@ -12,8 +12,12 @@ SRC_URI += "\
     file://domu-set-root-virtio.conf \
     ${@bb.utils.contains('DISTRO_FEATURES', 'sndbe', 'file://sndbe-backend.conf', '', d)} \
     ${@bb.utils.contains('DISTRO_FEATURES', 'displbe', 'file://displbe-backend.conf', '', d)} \
-    file://pvr-${XT_DOMU_CONFIG_NAME} \
 "
+
+python () {
+    if d.getVar('XT_DOMU_CONFIG_NAME'):
+        d.appendVar('SRC_URI', ' file://pvr-${XT_DOMU_CONFIG_NAME}')
+}
 
 FILES:${PN} += " \
     ${libdir}/xen/bin/domu-set-root \